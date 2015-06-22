cdfa0415954958e0029494e708140480f818e0b1
Kurt Schwehr <schwehr@google.com>
2015-06-22 09:10:07 -0700

    Simple empty comment tests

--- tests/write_test.cc
+++ tests/write_test.cc
@@ -14,9 +14,11 @@
 
 // Tests for writing out the basic record types.
 
+#include <sys/stat.h>
 #include <string>
 
 #include "gsf.h"
+#include "gsf_ft.h"
 #include "gsf_test_util.h"
 #include "gtest/gtest.h"
 
@@ -26,7 +28,7 @@ namespace {
 
 TEST(GsfWriteSimple, HeaderOnly) {
   int handle;
-  char filename[] = "sample01-header-only.gsf";
+  char filename[] = "sample-header-only.gsf";
   ASSERT_EQ(0, gsfOpen(filename, GSF_CREATE, &handle));
   ASSERT_EQ(0, gsfClose(handle));
   struct stat buf;
@@ -34,36 +36,44 @@ TEST(GsfWriteSimple, HeaderOnly) {
   ASSERT_EQ(24, buf.st_size);
 }
 
-TEST(GsfWriteSimple, CommentEmpty) {
-  int handle;
-  ASSERT_EQ(0, gsfOpen("sample-comment-empty.gsf", GSF_CREATE, &handle));
+void ValidateWriteComment(const char *filename,
+                          bool checksum,
+                          int expected_write_size,
+                          const char *comment,
+                          int expected_file_size) {
+  ASSERT_NE(nullptr, filename);
+  ASSERT_GE(expected_write_size, 20);
+  ASSERT_NE(nullptr, comment);
+  ASSERT_GE(expected_file_size, 44);
 
-  gsfDataID data_id = {false, 0, GSF_RECORD_COMMENT, 0};
+  int handle;
+  ASSERT_EQ(0, gsfOpen(filename, GSF_CREATE, &handle));
+
+  gsfDataID data_id = {checksum, 0, GSF_RECORD_COMMENT, 0};
   gsfRecords record;
   record.comment.comment_time.tv_sec = 1;
   record.comment.comment_time.tv_nsec = 2;
-  char comment[] = "";
   record.comment.comment_length = strlen(comment);
-  record.comment.comment = comment;
-  ASSERT_EQ(20, gsfWrite(handle, &data_id, &record));
+  record.comment.comment = const_cast<char *>(comment);
+  ASSERT_EQ(expected_write_size, gsfWrite(handle, &data_id, &record));
 
   ASSERT_EQ(0, gsfClose(handle));
+
+  struct stat buf;
+  ASSERT_EQ(0, stat(filename, &buf));
+  ASSERT_EQ(expected_file_size, buf.st_size);
 }
 
-TEST(GsfWriteSimple, CommentEmptyWithChecksum) {
-  int handle;
-  ASSERT_EQ(0, gsfOpen("sample-comment-empty.gsf", GSF_CREATE, &handle));
-
-  gsfDataID data_id = {true, 0, GSF_RECORD_COMMENT, 0};
-  gsfRecords record;
-  record.comment.comment_time.tv_sec = 1;
-  record.comment.comment_time.tv_nsec = 2;
+TEST(GsfWriteSimple, CommentEmpty) {
+  char filename[] = "sample-comment-empty.gsf";
   char comment[] = "";
-  record.comment.comment_length = strlen(comment);
-  record.comment.comment = comment;
-  ASSERT_EQ(24, gsfWrite(handle, &data_id, &record));
+  ValidateWriteComment(filename, false, 20, comment, 44);
+}
 
-  ASSERT_EQ(0, gsfClose(handle));
+TEST(GsfWriteSimple, CommentEmptyChecksum) {
+  char filename[] = "sample-comment-empty-checksum.gsf";
+  char comment[] = "";
+  ValidateWriteComment(filename, true, 24, comment, 48);
 }
 
 }  // namespace
