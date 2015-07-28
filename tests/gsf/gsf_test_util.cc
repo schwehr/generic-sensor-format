e3117d747eba470511f1ab1418ab68f1436f2d18
Kurt Schwehr <schwehr@google.com>
2015-07-28 13:14:21 -0700

    clang-format --style=Google

--- tests/gsf/gsf_test_util.cc
+++ tests/gsf/gsf_test_util.cc
@@ -44,7 +44,7 @@ TempDir::~TempDir() {
   if (preserve_) {
     return;
   }
-  DIR* dir = opendir(tmp_dir_.c_str());
+  DIR *dir = opendir(tmp_dir_.c_str());
   if (dir == nullptr) {
     perror(nullptr);
     return;
@@ -64,40 +64,38 @@ TempDir::~TempDir() {
 
 string RecordTypeStr(unsigned int record_id) {
   switch (record_id) {
-  case GSF_RECORD_HEADER:
-    return "GSF_RECORD_HEADER";
-  case GSF_RECORD_SWATH_BATHYMETRY_PING:
-    return "GSF_RECORD_SWATH_BATHYMETRY_PING";
-  case GSF_RECORD_SOUND_VELOCITY_PROFILE:
-    return "GSF_RECORD_SOUND_VELOCITY_PROFILE";
-  case GSF_RECORD_PROCESSING_PARAMETERS:
-    return "GSF_RECORD_PROCESSING_PARAMETERS";
-  case GSF_RECORD_SENSOR_PARAMETERS:
-    return "GSF_RECORD_SENSOR_PARAMETERS";
-  case GSF_RECORD_COMMENT:
-    return "GSF_RECORD_COMMENT";
-  case GSF_RECORD_HISTORY:
-    return "GSF_RECORD_HISTORY";
-  case GSF_RECORD_NAVIGATION_ERROR:
-    return "GSF_RECORD_NAVIGATION_ERROR";
-  case GSF_RECORD_SWATH_BATHY_SUMMARY:
-    return "GSF_RECORD_SWATH_BATHY_SUMMARY";
-  case GSF_RECORD_SINGLE_BEAM_PING:
-    return "GSF_RECORD_SINGLE_BEAM_PING";
-  case GSF_RECORD_HV_NAVIGATION_ERROR:
-    return "GSF_RECORD_HV_NAVIGATION_ERROR";
-  case GSF_RECORD_ATTITUDE:
-    return "GSF_RECORD_ATTITUDE";
+    case GSF_RECORD_HEADER:
+      return "GSF_RECORD_HEADER";
+    case GSF_RECORD_SWATH_BATHYMETRY_PING:
+      return "GSF_RECORD_SWATH_BATHYMETRY_PING";
+    case GSF_RECORD_SOUND_VELOCITY_PROFILE:
+      return "GSF_RECORD_SOUND_VELOCITY_PROFILE";
+    case GSF_RECORD_PROCESSING_PARAMETERS:
+      return "GSF_RECORD_PROCESSING_PARAMETERS";
+    case GSF_RECORD_SENSOR_PARAMETERS:
+      return "GSF_RECORD_SENSOR_PARAMETERS";
+    case GSF_RECORD_COMMENT:
+      return "GSF_RECORD_COMMENT";
+    case GSF_RECORD_HISTORY:
+      return "GSF_RECORD_HISTORY";
+    case GSF_RECORD_NAVIGATION_ERROR:
+      return "GSF_RECORD_NAVIGATION_ERROR";
+    case GSF_RECORD_SWATH_BATHY_SUMMARY:
+      return "GSF_RECORD_SWATH_BATHY_SUMMARY";
+    case GSF_RECORD_SINGLE_BEAM_PING:
+      return "GSF_RECORD_SINGLE_BEAM_PING";
+    case GSF_RECORD_HV_NAVIGATION_ERROR:
+      return "GSF_RECORD_HV_NAVIGATION_ERROR";
+    case GSF_RECORD_ATTITUDE:
+      return "GSF_RECORD_ATTITUDE";
   }
   return "UNKNOWN";
 }
 
 const gsfAttitude GsfAttitude(short num_measurements,
-                        const struct timespec *attitude_time,
-                        const double *pitch,
-                        const double *roll,
-                        const double *heave,
-                        const double *heading) {
+                              const struct timespec *attitude_time,
+                              const double *pitch, const double *roll,
+                              const double *heave, const double *heading) {
   gsfAttitude attitude;
   attitude.num_measurements = num_measurements;
   attitude.attitude_time = const_cast<struct timespec *>(attitude_time);
@@ -109,21 +107,17 @@ const gsfAttitude GsfAttitude(short num_measurements,
 }
 
 const gsfComment GsfComment(const struct timespec &when, const char *comment) {
-  gsfComment ret = {
-    { when.tv_sec, when.tv_nsec },
-    int(strlen(comment)),
-    const_cast<char *>(comment)
-  };
+  gsfComment ret = {{when.tv_sec, when.tv_nsec},
+                    int(strlen(comment)),
+                    const_cast<char *>(comment)};
   return ret;
 }
 
-const gsfHistory GsfHistory(const struct timespec &when,
-                      const char *host_name,
-                      const char *operator_name,
-                      const char *command_line,
-                      const char *comment) {
+const gsfHistory GsfHistory(const struct timespec &when, const char *host_name,
+                            const char *operator_name, const char *command_line,
+                            const char *comment) {
   gsfHistory history;
-  history.history_time = { when.tv_sec, when.tv_nsec };
+  history.history_time = {when.tv_sec, when.tv_nsec};
   strncpy(history.host_name, host_name, GSF_HOST_NAME_LENGTH);
   history.host_name[GSF_HOST_NAME_LENGTH] = '\0';
   strncpy(history.operator_name, operator_name, GSF_OPERATOR_LENGTH);
@@ -137,24 +131,19 @@ const gsfScaleFactors GsfScaleFactors(const vector<gsfScaleInfo> &scale_info) {
   assert(scale_info.size() <= GSF_MAX_PING_ARRAY_SUBRECORDS);
   gsfScaleFactors scale_factors;
   scale_factors.numArraySubrecords = scale_info.size();
-  for (vector<gsfScaleInfo>::size_type i=0; i < scale_info.size(); ++i) {
+  for (vector<gsfScaleInfo>::size_type i = 0; i < scale_info.size(); ++i) {
     scale_factors.scaleTable[i] = scale_info[i];
   }
-  for (int i=scale_info.size(); i < GSF_MAX_PING_ARRAY_SUBRECORDS; ++i) {
+  for (int i = scale_info.size(); i < GSF_MAX_PING_ARRAY_SUBRECORDS; ++i) {
     scale_factors.scaleTable[i] = {0, 0.0, 0.0};
   }
   return scale_factors;
 }
 
-const gsfSVP GsfSvp(
-     struct timespec observation_time,
-     struct timespec application_time,
-     double latitude,
-     double longitude,
-     int number_points,
-     const double *depth,
-     const double *sound_speed
-                    ) {
+const gsfSVP GsfSvp(struct timespec observation_time,
+                    struct timespec application_time, double latitude,
+                    double longitude, int number_points, const double *depth,
+                    const double *sound_speed) {
   gsfSVP svp;
   svp.observation_time = observation_time;
   svp.application_time = application_time;
@@ -167,55 +156,29 @@ const gsfSVP GsfSvp(
 }
 
 const gsfSwathBathyPing GsfSwathBathyPing(
-    struct timespec ping_time,
-    double latitude,
-    double longitude,
-    double height,
-    double sep,
-    short number_beams,
-    short center_beam,
-    unsigned short ping_flags,
-    short reserved,
-    double tide_corrector,
-    double gps_tide_corrector,
-    double depth_corrector,
-    double heading,
-    double pitch,
-    double roll,
-    double heave,
-    double course,
-    double speed,
-    gsfScaleFactors &scaleFactors,
-    const double *depth,
-    const double *nominal_depth,
-    const double *across_track,
-    const double *along_track,
-    const double *travel_time,
-    const double *beam_angle,
-    const double *mc_amplitude,
-    const double *mr_amplitude,
-    const double *echo_width,
-    const double *quality_factor,
-    const double *receive_heave,
-    const double *depth_error,
-    const double *across_track_error,
-    const double *along_track_error,
-    const unsigned char *quality_flags,
-    const unsigned char *beam_flags,
-    const double *signal_to_noise,
-    const double *beam_angle_forward,
-    const double *vertical_error,
-    const double *horizontal_error,
-    const unsigned short *sector_number,
-    const unsigned short *detection_info,
-    const double *incident_beam_adj,
-    const unsigned short *system_cleaning,
-    const double *doppler_corr,
+    struct timespec ping_time, double latitude, double longitude, double height,
+    double sep, short number_beams, short center_beam,
+    unsigned short ping_flags, short reserved, double tide_corrector,
+    double gps_tide_corrector, double depth_corrector, double heading,
+    double pitch, double roll, double heave, double course, double speed,
+    gsfScaleFactors &scaleFactors, const double *depth,
+    const double *nominal_depth, const double *across_track,
+    const double *along_track, const double *travel_time,
+    const double *beam_angle, const double *mc_amplitude,
+    const double *mr_amplitude, const double *echo_width,
+    const double *quality_factor, const double *receive_heave,
+    const double *depth_error, const double *across_track_error,
+    const double *along_track_error, const unsigned char *quality_flags,
+    const unsigned char *beam_flags, const double *signal_to_noise,
+    const double *beam_angle_forward, const double *vertical_error,
+    const double *horizontal_error, const unsigned short *sector_number,
+    const unsigned short *detection_info, const double *incident_beam_adj,
+    const unsigned short *system_cleaning, const double *doppler_corr,
     const double *sonar_vert_uncert,
-    int sensor_id // ,
-    // gsfSensorSpecific sensor_data,
-    // const gsfBRBIntensity *brb_inten
-  ) {
+    int sensor_id  // ,
+                   // gsfSensorSpecific sensor_data,
+                   // const gsfBRBIntensity *brb_inten
+    ) {
   gsfSwathBathyPing ping;
   ping.ping_time = ping_time;
   ping.latitude = latitude;
@@ -271,16 +234,16 @@ const gsfSwathBathyPing GsfSwathBathyPing(
 
 void PacketCounts::Verify(const vector<int> &expected) {
   ASSERT_EQ(NUM_REC_TYPES, expected.size());
-  for (int i=0; i < NUM_REC_TYPES; ++i) {
-    EXPECT_EQ(expected[i], counts_[i])
-        << "Mismatch. packet #" << i << " expected: " << expected[i]
-        << "  found: " << counts_[i] << "\n";
+  for (int i = 0; i < NUM_REC_TYPES; ++i) {
+    EXPECT_EQ(expected[i], counts_[i]) << "Mismatch. packet #" << i
+                                       << " expected: " << expected[i]
+                                       << "  found: " << counts_[i] << "\n";
   }
 }
 
 void VerifyAttitude(const gsfAttitude &expected, const gsfAttitude &actual) {
   EXPECT_EQ(expected.num_measurements, actual.num_measurements);
-  for (int i=0; i < expected.num_measurements; ++i) {
+  for (int i = 0; i < expected.num_measurements; ++i) {
     EXPECT_EQ(expected.attitude_time[i].tv_sec, actual.attitude_time[i].tv_sec);
     EXPECT_EQ(expected.attitude_time[i].tv_nsec,
               actual.attitude_time[i].tv_nsec);
@@ -310,7 +273,7 @@ void VerifyArray(const unsigned char *expected, const unsigned char *actual,
     ASSERT_EQ(nullptr, actual);
     return;
   }
-  for (int i=0; i < size; ++i) {
+  for (int i = 0; i < size; ++i) {
     EXPECT_EQ(actual[i], expected[i]);
   }
 }
@@ -326,13 +289,12 @@ void VerifyArray(const unsigned short *expected, const unsigned short *actual,
     ASSERT_EQ(nullptr, actual);
     return;
   }
-  for (int i=0; i < size; ++i) {
+  for (int i = 0; i < size; ++i) {
     EXPECT_EQ(actual[i], expected[i]);
   }
 }
 
-void VerifyArray(const double *expected, const double *actual,
-                 int size) {
+void VerifyArray(const double *expected, const double *actual, int size) {
   if (size == 0) {
     ASSERT_EQ(nullptr, expected);
     ASSERT_EQ(nullptr, actual);
@@ -342,7 +304,7 @@ void VerifyArray(const double *expected, const double *actual,
     ASSERT_EQ(nullptr, actual);
     return;
   }
-  for (int i=0; i < size; ++i) {
+  for (int i = 0; i < size; ++i) {
     EXPECT_EQ(actual[i], expected[i]);
   }
 }
@@ -396,7 +358,7 @@ void VerifySwathBathyPing(const gsfSwathBathyPing &expected,
   } else
 #endif
   {
-    for (int i=0; i < expected.scaleFactors.numArraySubrecords; ++i) {
+    for (int i = 0; i < expected.scaleFactors.numArraySubrecords; ++i) {
       EXPECT_EQ(expected.scaleFactors.scaleTable[i].compressionFlag,
                 actual.scaleFactors.scaleTable[i].compressionFlag);
       EXPECT_DOUBLE_EQ(expected.scaleFactors.scaleTable[i].multiplier,
@@ -448,7 +410,6 @@ void VerifySwathBathyPing(const gsfSwathBathyPing &expected,
 
 void VerifySwathBathySummary(const gsfSwathBathySummary &expected,
                              const gsfSwathBathySummary &actual) {
-
   EXPECT_EQ(expected.start_time.tv_sec, actual.start_time.tv_sec);
   EXPECT_EQ(expected.start_time.tv_nsec, actual.start_time.tv_nsec);
   EXPECT_EQ(expected.end_time.tv_sec, actual.end_time.tv_sec);
@@ -461,16 +422,14 @@ void VerifySwathBathySummary(const gsfSwathBathySummary &expected,
   EXPECT_DOUBLE_EQ(expected.max_depth, actual.max_depth);
 }
 
-void VerifyComment(const gsfComment &expected,
-                   const gsfComment &actual) {
+void VerifyComment(const gsfComment &expected, const gsfComment &actual) {
   EXPECT_EQ(expected.comment_time.tv_sec, actual.comment_time.tv_sec);
   EXPECT_EQ(expected.comment_time.tv_nsec, actual.comment_time.tv_nsec);
   EXPECT_EQ(expected.comment_length, actual.comment_length);
   EXPECT_STREQ(expected.comment, actual.comment);
 }
 
-void VerifyHistory(const gsfHistory &expected,
-                   const gsfHistory &actual) {
+void VerifyHistory(const gsfHistory &expected, const gsfHistory &actual) {
   EXPECT_EQ(expected.history_time.tv_sec, actual.history_time.tv_sec);
   EXPECT_EQ(expected.history_time.tv_nsec, actual.history_time.tv_nsec);
   EXPECT_STREQ(expected.host_name, actual.host_name);
@@ -479,12 +438,12 @@ void VerifyHistory(const gsfHistory &expected,
   EXPECT_STREQ(expected.comment, actual.comment);
 }
 
-std::ostream& operator<< (std::ostream &o, const gsfBRBIntensity &intensity) {
+std::ostream &operator<<(std::ostream &o, const gsfBRBIntensity &intensity) {
   o << "GsfBrbIntensity(\n"
     << "  " << intensity.bits_per_sample << ",\n"
     << "  " << intensity.applied_corrections << ",\n"
     << "  {\n";
-  for (int i=0; i < 15; ++i) {
+  for (int i = 0; i < 15; ++i) {
     o << "    " << intensity.spare[i] << ",\n";
   }
   o << "    " << intensity.spare[15] << "\n"
@@ -502,11 +461,11 @@ std::ostream& operator<< (std::ostream &o, const gsfBRBIntensity &intensity) {
   return o;
 }
 
-std::ostream& operator<< (std::ostream &o, const gsfScaleFactors &factors) {
+std::ostream &operator<<(std::ostream &o, const gsfScaleFactors &factors) {
   o << "GsfScaleFactors(\n"
     << "  " << factors.numArraySubrecords << ",\n"
     << "  {\n";
-  for (int i=0; i < factors.numArraySubrecords - 1; ++i) {
+  for (int i = 0; i < factors.numArraySubrecords - 1; ++i) {
     o << "    " << factors.scaleTable[i] << ",\n";
   }
   if (factors.numArraySubrecords > 0) {
@@ -516,19 +475,16 @@ std::ostream& operator<< (std::ostream &o, const gsfScaleFactors &factors) {
   return o;
 }
 
-std::ostream& operator<< (std::ostream &o, const gsfScaleInfo &scale_info) {
-  o << "GsfScaleInfo("
-    << static_cast<int>(scale_info.compressionFlag) << ", "
-    << scale_info.multiplier << ", "
-    << scale_info.offset
-    << ")";
+std::ostream &operator<<(std::ostream &o, const gsfScaleInfo &scale_info) {
+  o << "GsfScaleInfo(" << static_cast<int>(scale_info.compressionFlag) << ", "
+    << scale_info.multiplier << ", " << scale_info.offset << ")";
   return o;
 }
 
-std::ostream& operator<< (std::ostream &o, const gsfSwathBathyPing &ping) {
+std::ostream &operator<<(std::ostream &o, const gsfSwathBathyPing &ping) {
   o << "GsfSwathBathyPing(\n"
-    << "  {" << ping.ping_time.tv_sec << ", "
-    << ping.ping_time.tv_nsec << "},\n"
+    << "  {" << ping.ping_time.tv_sec << ", " << ping.ping_time.tv_nsec
+    << "},\n"
     << "  " << ping.latitude << ",  // latitude\n"
     << "  " << ping.longitude << ",  // longitude\n"
     << "  " << ping.height << ",  // \n"
@@ -545,8 +501,7 @@ std::ostream& operator<< (std::ostream &o, const gsfSwathBathyPing &ping) {
     << "  " << ping.roll << ",  // \n"
     << "  " << ping.heave << ",  // \n"
     << "  " << ping.course << ",  // \n"
-    << "  " << ping.speed << ",  // \n"
-    << ping.scaleFactors << ",\n";
+    << "  " << ping.speed << ",  // \n" << ping.scaleFactors << ",\n";
 
   WriteArray(o, ping.number_beams, ping.depth, "depth");
   WriteArray(o, ping.number_beams, ping.nominal_depth, "nominal_depth");
@@ -560,12 +515,14 @@ std::ostream& operator<< (std::ostream &o, const gsfSwathBathyPing &ping) {
   WriteArray(o, ping.number_beams, ping.quality_factor, "quality_factor");
   WriteArray(o, ping.number_beams, ping.receive_heave, "receive_heave");
   WriteArray(o, ping.number_beams, ping.depth_error, "depth_error");
-  WriteArray(o, ping.number_beams, ping.across_track_error, "across_track_error");
+  WriteArray(o, ping.number_beams, ping.across_track_error,
+             "across_track_error");
   WriteArray(o, ping.number_beams, ping.along_track_error, "along_track_error");
   WriteArray(o, ping.number_beams, ping.quality_flags, "quality_flags");
   WriteArray(o, ping.number_beams, ping.beam_flags, "beam_flags");
   WriteArray(o, ping.number_beams, ping.signal_to_noise, "signal_to_noise");
-  WriteArray(o, ping.number_beams, ping.beam_angle_forward, "beam_angle_forward");
+  WriteArray(o, ping.number_beams, ping.beam_angle_forward,
+             "beam_angle_forward");
   WriteArray(o, ping.number_beams, ping.vertical_error, "vertical_error");
   WriteArray(o, ping.number_beams, ping.horizontal_error, "horizontal_error");
   WriteArray(o, ping.number_beams, ping.sector_number, "sector_number");
@@ -576,7 +533,8 @@ std::ostream& operator<< (std::ostream &o, const gsfSwathBathyPing &ping) {
   WriteArray(o, ping.number_beams, ping.sonar_vert_uncert, "sonar_vert_uncert");
   o << "  " << ping.sensor_id << ",  // sensor_id.\n";
   // TODO(schwehr) gsfSensorSpecific sensor_data.
-  // WriteArray(o, ping.number_beams, ping.brb_inten, "brb_inten");  // gsfBRBIntensity
+  // WriteArray(o, ping.number_beams, ping.brb_inten, "brb_inten");  //
+  // gsfBRBIntensity
 
   o << ");\n\n";
   return o;
@@ -591,11 +549,12 @@ void WriteArray(std::ostream &o, size_t size, const unsigned char *data,
     assert(size > 0);
 
     o << "  {\n";
-    for (size_t i=0; i < size - 1; ++i) {
+    for (size_t i = 0; i < size - 1; ++i) {
       o << "  " << int(data[i]) << ",\n";
     }
     o << "    " << int(data[size - 1]) << "\n"
-      << "  }," << "  // " << name << ".\n";
+      << "  },"
+      << "  // " << name << ".\n";
   }
 }
 
@@ -607,11 +566,12 @@ void WriteArray(std::ostream &o, size_t size, const unsigned short *data,
     assert(size > 0);
 
     o << "  {\n";
-    for (size_t i=0; i < size - 1; ++i) {
+    for (size_t i = 0; i < size - 1; ++i) {
       o << "  " << data[i] << ",\n";
     }
     o << "    " << data[size - 1] << "\n"
-      << "  }," << "  // " << name << ".\n";
+      << "  },"
+      << "  // " << name << ".\n";
   }
 }
 
@@ -623,7 +583,7 @@ void WriteArray(std::ostream &o, size_t size, const double *data,
     assert(size > 0);
 
     o << "  // " << name << ".\n  {\n";
-    for (size_t i=0; i < size - 1; ++i) {
+    for (size_t i = 0; i < size - 1; ++i) {
       o << "  " << data[i] << ",\n";
     }
     o << "    " << data[size - 1] << "\n"
